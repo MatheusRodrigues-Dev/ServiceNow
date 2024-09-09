77c05e0f4ae6b693dcd8c6981b5a82d4b3b2d38c
MatheusRodrigues-Dev <rodriguesmatheus309@gmail.com>
2024-09-09 15:11:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ServiceNow',
+  webDir: 'www'
+};
+
+export default config;
